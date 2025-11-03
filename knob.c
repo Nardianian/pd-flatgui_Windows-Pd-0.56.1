9ab9225772c105d709e6570880a8849058cfd59e
Nardianian <145938851+Nardianian@users.noreply.github.com>
2025-11-03 18:59:00 +0100

    Update knob.c

--- knob.c
+++ knob.c
@@ -314,25 +314,24 @@ static void knob_getrect(t_gobj *z, t_glist *glist,
     *yp2 = *yp1 + x->x_gui.x_h + 5;
 }
 
-static void knob_save(t_gobj *z, t_binbuf *b)
-{
-    t_knob *x = (t_knob *)z;
-    int bflcol[3];
+static void knob_save(t_gobj *z, t_binbuf *b) 
+{ 
+    t_knob *x = (t_knob *)z; 
     t_symbol *srl[3];
+    t_symbol *bflcol[3]; 
 
-    iemgui_save(&x->x_gui, srl, bflcol);
-    binbuf_addv(b, "ssiisiiffiisssiiiiiiiii", gensym("#X"),gensym("obj"),
-		(t_int)x->x_gui.x_obj.te_xpix, (t_int)x->x_gui.x_obj.te_ypix,
-		atom_getsymbol(binbuf_getvec(x->x_gui.x_obj.te_binbuf)),
-        x->x_gui.x_h, x->x_gui.x_h,
-		(float)x->x_min, (float)x->x_max,
-		x->x_lin0_log1, iem_symargstoint(&x->x_gui.x_isa),
-		srl[0], srl[1], srl[2],
-		x->x_gui.x_ldx, x->x_gui.x_ldy,
-		iem_fstyletoint(&x->x_gui.x_fsf), x->x_gui.x_fontsize,
-		bflcol[0], bflcol[1], bflcol[2],
-		x->x_val, x->x_steady);
-    binbuf_addv(b, ";");
+    iemgui_save(&x->x_gui, srl, bflcol); 
+    binbuf_addv(b, "ssiisiiiiiiiii",
+                gensym("#X"), gensym("obj"), 
+                (int)x->x_gui.x_obj.te_xpix, 
+                (int)x->x_gui.x_obj.te_ypix,
+                gensym("knob"), 
+                (int)x->x_min, (int)x->x_max, 
+                (int)x->x_val, (int)x->x_lin0_log1, 
+                (int)x->x_steady, 
+                x->x_gui.x_w, x->x_gui.x_h, 
+                srl[0], srl[1], srl[2], 
+                bflcol[0], bflcol[1], bflcol[2]); 
 }
 
 void knob_check_height(t_knob *x, int h)
